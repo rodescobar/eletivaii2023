b340151883a11dc7cd31b62e8fde0498941e6204
Rodrigo Escobar <rodescobar@gmail.com>
2023-05-09 17:36:58 -0300

    Vue

---
+++ Aula13-Vue/Aula02/src/components/PrimeiroComponente.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Rodrigo Escobar</h1>
+    <br />
+    <p>Olá mundo</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PrimeiroComponente",
+};
+</script>
